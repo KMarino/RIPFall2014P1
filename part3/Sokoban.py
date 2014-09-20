d18951ca46a3bf6dec21867d45cc6d8d7d4fdf8e
pbhatnagar3 <pbhatnagar3@gatech.edu>
2014-09-20 15:33:48 -0400

    added the moving functionality for the robot

--- part3/Sokoban.py
+++ part3/Sokoban.py
@@ -16,7 +16,44 @@ class Robot:
 		print "robot row number is ", self.row
 		print "robot col number is", self.col
 
-	def moveBox(self, box):
+	def move(self, row, col):
+		sokoban_problem.world_map[self.row * sokoban_problem.number_of_columns + self.col] = 'E'
+		sokoban_problem.world_map[row * sokoban_problem.number_of_columns + col] = 'R'
+		self.row = row
+		self.col = col
+
+	def move_itself(self, direction):
+		if direction == 'L':
+			if sokoban_problem.isAccessible(self.row, self.col - 1):
+				self.move(self.row, self.col - 1)
+				return True
+			else:
+				return False
+		if direction == 'R':
+			if sokoban_problem.isAccessible(self.row, self.col + 1):
+				self.move(self.row, self.col + 1)
+				return True
+			else:
+				return False
+		if direction == 'U':
+			if sokoban_problem.isAccessible(self.row -1, self.col):
+				self.move(self.row -1, self.col)
+				return True
+			else:
+				return False
+		if direction == 'D':
+			if sokoban_problem.isAccessible(self.row + 1, self.col):
+				self.move(self.row + 1, self.col)
+				return True
+			else:
+				return False
+		else:
+			print "INVALID DIRECTION"
+			return False
+
+				
+
+	def move_box(self, box):
 		self.print_robot_info()
 		print 
 		box.print_box_info()
@@ -101,13 +138,25 @@ class Sokoban:
     		return False
     	if col >= self.number_of_columns or col < 0:
     		return False	
-    	return self.world_map[int(row) * int(self.number_of_columns) + int(col)] != 'W'
+    	return self.world_map[int(row) * int(self.number_of_columns) + int(col)] != 'W' and self.world_map[int(row) * int(self.number_of_columns) + int(col)] != 'B'
 
 file_number = raw_input("enter file number : ")
 sokoban_problem = Sokoban(1, file_number)	
 sokoban_problem.print_world_map()
-if sokoban_problem.robot.moveBox(sokoban_problem.boxes[0]) == True:
+if sokoban_problem.robot.move_box(sokoban_problem.boxes[0]) == True:
 	print "BOX MOVED"
 else:
-	print "BOX NOT MOVED"	
-sokoban_problem.print_world_map()
+	print "BOX NOT MOVED"
+print sokoban_problem.print_world_map()
+directions = ['R', 'L', 'U', 'D'];
+for d in directions:
+	print '*'*10	
+	print "here is the initial world condition"
+	sokoban_problem.print_world_map()
+	print "now the robot will try to move in ", d , " direction"
+	if sokoban_problem.robot.move_itself(d):
+		print "ROBOT MOVED"
+	else:
+		print "ROBOT COULD NOT MOVE IN ", d, " DIRECTION"			
+	print "this is the condition of the world after robot tried to move"
+	sokoban_problem.print_world_map()
